a4d8d2abac95e684854ba75c3fd00a454322aa23
Bilibili_MengChu <2133404320@qq.com>
2024-04-15 23:46:46 +0800

    Fix

--- TheOtherRoles/GlobalUsing.cs
+++ TheOtherRoles/GlobalUsing.cs
@@ -1,4 +1,6 @@
-﻿global using HarmonyLib;
+﻿extern alias JetBrains;
+
+global using HarmonyLib;
 global using Il2CppInterop.Runtime;
 global using Il2CppInterop.Runtime.Attributes;
 global using Il2CppInterop.Runtime.Injection;
@@ -6,5 +8,8 @@ global using Il2CppInterop.Runtime.InteropTypes;
 global using Il2CppInterop.Runtime.InteropTypes.Arrays;
 global using TheOtherRoles.Helper;
 global using TheOtherRoles.Utilities;
+global using static TheOtherRoles.Modules.Languages.LanguageExtension;
+global using UsedImplicitly = JetBrains::JetBrains.Annotations.UsedImplicitlyAttribute;
+global using MeansImplicitUse = JetBrains::JetBrains.Annotations.MeansImplicitUseAttribute;
 global using static TheOtherRoles.Helper.LogHelper;
 global using Main = TheOtherRoles.TheOtherRolesPlugin;
